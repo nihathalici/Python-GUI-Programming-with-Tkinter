e8f2c7c9f3937910cfd1b6de746c2b0ed1e8379d
Nihat Halici <nihat.halici@gmail.com>
2022-05-19 14:30:38 +0200

    Update hello_tkinter.py

--- CHPT-01/hello_tkinter.py
+++ CHPT-01/hello_tkinter.py
@@ -1,3 +1,3 @@
 """Hello World application for Tkinter"""
 #
-https://github.com/PacktPublishing/Python-GUI-Programming-with-Tkinter-2E/blob/main/Chapter01/hello_tkinter.py
+# https://github.com/PacktPublishing/Python-GUI-Programming-with-Tkinter-2E/blob/main/Chapter01/hello_tkinter.py
