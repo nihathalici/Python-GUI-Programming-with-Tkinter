dc586ff117a737ab02c52c05fb3db7ace25efac4
Nihat Halici <nihat.halici@gmail.com>
2022-05-23 07:48:36 +0200

    Delete CHPT-01 directory

--- CHPT-01/banana_survey.py
+++
@@ -1,3 +0,0 @@
-"""A banana preferences survey written in Python with Tkinter"""
-#
-# https://github.com/PacktPublishing/Python-GUI-Programming-with-Tkinter-2E/blob/main/Chapter01/banana_survey_variables.py
--- CHPT-01/hello_tkinter.py
+++
@@ -1,3 +0,0 @@
-"""Hello World application for Tkinter"""
-#
-# https://github.com/PacktPublishing/Python-GUI-Programming-with-Tkinter-2E/blob/main/Chapter01/hello_tkinter.py
