b2a53210460c8c415052d5041807a28fd78c6a40
Nihat Halici <nihat.halici@gmail.com>
2022-07-22 07:21:06 +0200

    Update application.py

--- CHPT-06-Planning-for-the-Expansion-of-Our-Application/ABQ_Data_Entry/abq_data_entry/application.py
+++ CHPT-06-Planning-for-the-Expansion-of-Our-Application/ABQ_Data_Entry/abq_data_entry/application.py
@@ -1,1 +1,3 @@
 """The application/controller class for ABQ Data Entry"""
+
+# https://github.com/PacktPublishing/Python-GUI-Programming-with-Tkinter-2E/blob/main/Chapter06/ABQ_Data_Entry/abq_data_entry/application.py
